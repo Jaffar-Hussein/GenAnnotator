eebde35c8966e0cbb46f8ac20817338bd001401f
Jaffar Hussein <jaffhussein@icloud.com>
2024-12-04 11:12:28 +0100

    Enhance build script to ensure pip is installed and upgrade dependencies

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Install dependencies
+# Ensure pip is installed
+python3.9 -m ensurepip --upgrade
+
+# Upgrade pip and install dependencies
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
