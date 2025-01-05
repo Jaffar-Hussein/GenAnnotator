88cbb6e85b5cadc831f53f8c59f94013c8ca5f51
Leo <leo.jourdain@universite-paris-saclay.fr>
2025-01-05 20:16:00 +0100

    [UPDATE] ADD User role and update signup view

--- signup/views.py
+++ signup/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
