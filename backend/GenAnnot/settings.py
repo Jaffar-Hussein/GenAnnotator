9b646ef3b73d38a635375a58454d99b569e37fa3
Jaffar Hussein <jaffhussein@icloud.com>
2025-01-08 21:18:21 +0100

    [UPDATE] add CORS headers support and update allowed origins

--- backend/GenAnnot/settings.py
+++ backend/GenAnnot/settings.py
@@ -42,6 +42,7 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -60,15 +61,14 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     'dj_rest_auth',
     'dj_rest_auth.registration',
-    "corsheaders",
     "phonenumber_field",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -93,6 +93,7 @@ REST_AUTH = {
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 # JWT Settings
