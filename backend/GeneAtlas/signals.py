3f1de813cfd36bf69004bd2d17f5fad446beb066
Leo <leo.jourdain@universite-paris-saclay.fr>
2025-01-14 20:13:38 +0100

    [UPDATE] Add RAW status for annotation / Signals to update genome annotation status

--- backend/GeneAtlas/signals.py
+++ backend/GeneAtlas/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, pre_save
+from django.db.models import Count
 from django.dispatch import receiver
-from .models import Gene, GeneAnnotationStatus, GeneAnnotation, PeptideAnnotation
+from .models import Genome, Gene, GeneAnnotationStatus, GeneAnnotation, PeptideAnnotation
 
 @receiver(post_save, sender=Gene)
 def create_gene_status(sender, instance, created, **kwargs):
@@ -26,16 +27,43 @@ def handle_status_change(sender, instance, **kwargs):
     except GeneAnnotationStatus.DoesNotExist:
         pass
 
+@receiver(post_save, sender=GeneAnnotationStatus)
+def update_genome_status(sender, instance, created, **kwargs):
+    """Check if genome is fully annotated"""
+    try:
+        if isinstance(instance, GeneAnnotationStatus):
+            genome = Genome.objects.get(pk=instance.gene.genome.pk)
+            if not created:
+                print("GeneAnnotationStatus updated")
+                if instance.status == GeneAnnotationStatus.REJECTED or instance.status == GeneAnnotationStatus.APPROVED:
+                    status_counts = GeneAnnotationStatus.objects.values(
+                        'gene__genome', 
+                        'status'
+                    ).filter(gene__genome=genome).annotate(
+                        count=Count('status')
+                    )
+                    if len(status_counts) == 1 and status_counts[0]['status'] == GeneAnnotationStatus.APPROVED:
+                        genome.annotation = True
+                    else:
+                        genome.annotation = False
+            elif created:
+                genome.annotation = False
+            genome.sequence = genome.get_sequence().encode('utf-8')
+            genome.save()
+    except Genome.DoesNotExist:
+        pass
+    
+
 @receiver(post_save, sender=GeneAnnotation)
 #@receiver(post_save, sender=PeptideAnnotation)
 def reset_rejected_status_if_updated(sender, instance, created, **kwargs):
     """
-    If an annotation is added/updated and its status was REJECTED, reset it to PENDING.
+    If an annotation is added/updated and its status was REJECTED / SUBMITTED, reset it to RAW.
     """
     try:
         existing_status = GeneAnnotationStatus.objects.get(gene=instance.gene_instance)
 
-        if existing_status.status == GeneAnnotationStatus.REJECTED:
+        if existing_status.status == GeneAnnotationStatus.REJECTED or existing_status.status == GeneAnnotationStatus.PENDING:
             existing_status.reset()
 
     except GeneAnnotationStatus.DoesNotExist:
