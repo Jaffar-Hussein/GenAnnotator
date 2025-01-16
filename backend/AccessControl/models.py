aa2bffe21a4352dbc341fb7018a095fdd7fc2e3c
Leo <leo.jourdain@universite-paris-saclay.fr>
2025-01-16 19:41:19 +0100

    [UPDATE] Add commentaries for AccessControl App

--- backend/AccessControl/models.py
+++ backend/AccessControl/models.py
@@ -5,6 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class CustomUser(AbstractUser):
 
+    # Set role of user
     def setrole(self, role):
         if role in [self.reader, self.annotator, self.validator]:
             self.role = role
@@ -13,9 +14,10 @@ class CustomUser(AbstractUser):
         else:
             return False
 
-    # Additional fields
     email = models.EmailField(max_length=254, unique=True, blank=False, null=False, validators=[validate_email])
     phone_number = PhoneNumberField(blank=True)
+
+    # User roles
     reader = "READER"
     annotator = "ANNOTATOR"
     validator = "VALIDATOR"
@@ -25,7 +27,9 @@ class CustomUser(AbstractUser):
         (validator, 'Validator'),
     ]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='READER')
+
     last_login_time = models.DateTimeField(auto_now=True)
 
+    # Represent user as
     def __str__(self):
         return f"{self.username}, {self.email}, {self.role}"
--- backend/AccessControl/views.py
+++ backend/AccessControl/views.py
@@ -26,9 +26,15 @@ class SignUpView(CreateView):
     template_name = "registration/signup.html"
 
 class SignupAPIView(APIView):
+
+    # Everyone can access this view
+
+    # Method for POST requests
     def post(self, request, *args, **kwargs):
         serializer = SignUpSerializer(data=request.data)
+        # Check if the serializer is valid
         if serializer.is_valid():
+            # Create a new user with the validated data
             user = CustomUser(
                 username=serializer.validated_data['username'],
                 email=serializer.validated_data['email'],
@@ -36,22 +42,36 @@ class SignupAPIView(APIView):
                 first_name=serializer.validated_data['first_name'],
                 last_name=serializer.validated_data['last_name'],
             )
+            # Use the set_password method to hash the password
             user.set_password(serializer.validated_data['password'])
+            # Save the user
             user.save()
+            # Send email confirmation
             send_email_confirmation(request, user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # If the serializer is not valid, return the errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class LoginAPIView(APIView):
 
+    # Everyone can access this view
+
+    # Method for POST requests
     def post(self, request, *args, **kwargs):
+        # Serialize the request data
         serializer = LoginSerializer(data=request.data)
+        # Check if the serializer is valid
         if serializer.is_valid():
+            # Get the username and password from the validated data
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
+            # Authenticate the user
             user = authenticate(username=username, password=password)
+            # If authentication is successful, a user is returned
             if user is not None:
+                # Create a token for the user
                 refresh = RefreshToken.for_user(user)
+                # Send back the user data and the token
                 return Response({
                     'username': user.username,
                     'email': user.email,
@@ -64,9 +84,12 @@ class LoginAPIView(APIView):
                     'refresh': str(refresh),
                     'access': str(refresh.access_token),
                 }, status=status.HTTP_200_OK)
+            # If authentication is not successful, return an error
             return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    # Methods not allowed
+
     def get(self, request, *args, **kwargs):
         return Response({"error": "GET request not supported."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
@@ -78,25 +101,44 @@ class LoginAPIView(APIView):
     
 class UserAPIView(APIView):
 
+    # Authentificated admin users can access this view
+
+    # Permission classes
     permission_classes = [IsAuthenticated|IsAdminUser]
 
+    # Method for GET requests
     def get(self, request, *args, **kwargs):
+        # Get all users
         users = CustomUser.objects.all()
+
+        # Pagination
         paginator = LimitOffsetPagination()
         users = paginator.paginate_queryset(users, request)
+
         serializer = UserSerializer(users, many=True)
+
         return paginator.get_paginated_response(serializer.data)
 
+    # Method for PUT requests
     def put(self, request, user, *args, **kwargs):
+        # Get the user requested
         user = CustomUser.objects.get(username=user)
+        # Get the new role to set
         new_role = request.data.get('role',None)
+        # If role is provided, set the new role
         if new_role:
+            # Call the setrole method from the user model
             if user.setrole(new_role):
+                # Response if the role is updated
                 return Response({"success": f"Role updated to {new_role}."}, status=status.HTTP_200_OK)
             else:
+                # Response if the role provided is invalid
                 return Response({"error": f"Invalid role provided {new_role}."}, status=status.HTTP_400_BAD_REQUEST)
         else:
+            # Response if the role is not provided in the request
             return Response({"error": "Role not provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Methods not allowed
 
     def post(self, request, *args, **kwargs):
         return Response({"error": "POST request not supported."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
