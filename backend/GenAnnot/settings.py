0a3dc9557151be8f8d55e7046d23770915558bbc
Leo <leo.jourdain@universite-paris-saclay.fr>
2025-01-23 13:05:14 +0100

    [ADD] docker-compose

--- backend/GenAnnot/settings.py
+++ backend/GenAnnot/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh","localhost","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh","localhost","127.0.0.1","0.0.0.0"]
 
 SITE_ID = 1
 
